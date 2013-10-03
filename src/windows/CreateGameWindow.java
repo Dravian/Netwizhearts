487202759469f348119a512925899b9fa9c9a9ab
Patrick Kubin <kubin@fim.uni-passau.de>
2013-10-04 01:59:25 +0200

    Erster Streich ;P

--- src/windows/CreateGameWindow.java
+++ src/windows/CreateGameWindow.java
@@ -2,10 +2,22 @@ package windows;
 
 import java.awt.EventQueue;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+
+import java.awt.Graphics;
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class CreateGameWindow extends JFrame {
 
@@ -13,7 +25,61 @@ public class CreateGameWindow extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+	private JTextField textField;
+	private BufferedImage image;
+
+	/**
+	 * Create the frame.
+	 * @throws IOException 
+	 */
+	public CreateGameWindow() throws IOException {
+		setTitle("Game Creation");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 390, 282);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Select Game");
+		lblNewLabel.setBounds(12, 12, 95, 24);
+		getContentPane().add(lblNewLabel);
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		comboBox.setBounds(12, 39, 188, 24);
+		comboBox.addItem("Wizard");
+		getContentPane().add(comboBox);
+		
+		JCheckBox chckbxPassword = new JCheckBox("Password:");
+		chckbxPassword.setBounds(12, 120, 117, 23);
+		getContentPane().add(chckbxPassword);
+		
+		JButton btnNewButton = new JButton("Leave");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(12, 215, 117, 25);
+		getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(12, 151, 188, 32);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+
+	    image = ImageIO.read(new File("src/wizard.jpg"));
+		
+		JPanel panel = new JPanel() {
+			protected void paintComponent(Graphics g) {
+			        super.paintComponent(g);
+			        g.drawImage(image, 0, 0, null);          
+			    }
+		};
+
+		panel.setBounds(235, 12, 139, 171);
+		getContentPane().add(panel);
+		
+		JButton btnNewButton_1 = new JButton("Create");
+		btnNewButton_1.setBounds(257, 215, 117, 25);
+		getContentPane().add(btnNewButton_1);
+	}
 
 	/**
 	 * Launch the application.
@@ -29,25 +95,5 @@ public class CreateGameWindow extends JFrame {
 				}
 			}
 		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public CreateGameWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblGameName = new JLabel("Game Name");
-		lblGameName.setBounds(10, 11, 77, 19);
-		contentPane.add(lblGameName);
-		
-		JLabel lblGameType = new JLabel("Game Type");
-		lblGameType.setBounds(10, 41, 77, 19);
-		contentPane.add(lblGameType);
 	}
 }
