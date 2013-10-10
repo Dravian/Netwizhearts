71f0399abafc1508e32a033813b3f248b52454ba
Patrick Kubin <kubin@fim.uni-passau.de>
2013-10-10 16:35:46 +0200

    Fenster vergessen und bissl im Pflichtenheft gemacht

--- src/windows/StartWindow.java
+++ src/windows/StartWindow.java
@@ -8,6 +8,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
 
 public class StartWindow extends JFrame {
 
@@ -42,33 +44,42 @@ public class StartWindow extends JFrame {
 		setTitle("Login");
 		setAlwaysOnTop(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 253, 161);
+		setBounds(100, 100, 280, 169);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblHostIp = new JLabel("Host IP:");
-		lblHostIp.setBounds(25, 55, 66, 14);
+		lblHostIp.setBounds(13, 71, 79, 23);
 		contentPane.add(lblHostIp);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(113, 27, 104, 20);
+		textField_1.setBounds(109, 45, 155, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNickname = new JLabel("Nickname:");
-		lblNickname.setBounds(25, 29, 78, 14);
+		lblNickname.setBounds(13, 43, 79, 23);
 		contentPane.add(lblNickname);
 		
 		textField = new JTextField();
-		textField.setBounds(113, 53, 104, 20);
+		textField.setBounds(109, 73, 155, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnConnect = new JButton("Connect");
-		btnConnect.setBounds(113, 85, 104, 23);
+		btnConnect.setBounds(109, 105, 104, 23);
 		contentPane.add(btnConnect);
+		
+		JLabel lblNewLabel = new JLabel("Language:");
+		lblNewLabel.setBounds(13, 8, 79, 23);
+		contentPane.add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"English", "Deutsch", "Boarisch"}));
+		comboBox.setSelectedIndex(0);
+		comboBox.setBounds(109, 7, 104, 24);
+		contentPane.add(comboBox);
 	}
-
 }
