90f2c3ed458864bbcdb04fdf1a7e0a1059134edb
alinamx <alina@meixl.de>
2013-11-10 12:30:52 +0100

    Änderungen ComObjects

--- src/ComObjects/RulesetMessage.java
+++ src/ComObjects/RulesetMessage.java
@@ -7,22 +7,26 @@ import java.io.Serializable;
 
 /**
  * Diese Klasse ist eine Verfeinerung der ComRuleset-Klasse.
- * Sie enth�lt einen Nachrichtentyp und vererbt an alle Nachrichten f�r das Regelwerk.
+ * Sie enthält einen Nachrichtentyp und vererbt an alle Nachrichten für das Regelwerk.
  */
 public abstract class RulesetMessage implements Serializable {
 
     /**
-     * Hier fehlt noch ein Kommentar
-     * @param serverRuleset
-     * @param name
+     * Diese Methode ist nötig, damit das ServerRuleset entscheiden kann
+     * welche Message es enthält und wie diese verarbeitet werden soll.
+     * @param serverRuleset ist das Ruleset, welches übergeben wird, damit
+     *                      die überladene Methode richtig gewählt wird.
+     * @param name          ist der Name des Spielers.
      */
     public void visit(ServerRuleset serverRuleset, String name) {
         serverRuleset.resolveMessage(this, name);
     }
 
     /**
-     * Hier fehlt noch ein Kommentar
-     * @param clientRuleset
+     * Diese Methode ist nötig, damit das ServerRuleset entscheiden kann
+     * welche Message es enthält und wie diese verarbeitet werden soll.
+     * @param clientRuleset ist das Ruleset, welches übergeben wird, damit
+     *                      die überladene Methode richtig gewählt wird.
      */
     public void visit(ClientRuleset clientRuleset) {
         clientRuleset.resolveMessage(this);
