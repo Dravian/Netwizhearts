001a4f76733c29d8ee5d01fc433002e17e913ab4
alinamx <alina@meixl.de>
2013-11-12 12:10:10 +0100

    Color statt int

--- src/ComObjects/MsgSelection.java
+++ src/ComObjects/MsgSelection.java
@@ -4,29 +4,28 @@ import Ruleset.Colour;
 
 /**
  * Diese Klasse ist eine Verfeinerung der RulesetMessage-Klasse.
- * Diese Nachricht enthält Information über eine Auswahl, die der
- * Spieler getroffen hat. Die Wahlmöglichkeiten werden durch Integer
- * repräsentiert.
+ * Diese Nachricht enth�lt eine Kartenfarbe, die der
+ * Spieler zuvor in einem Drop-Down Men� ausgewählt hat.
  */
 public class MsgSelection extends RulesetMessage {
 
-	private int selection;
+	private Colour selection;
 
     /**
-     * Dies ist der Kontruktor für eine neue MsgSelection-Nachricht.
-     * @param selection ist die getroffene Auswahl, repräsentiert
-     *                  durch einen Integer.
+     * Dies ist der Kontruktor f�r eine neue MsgSelection-Nachricht.
+     * @param selection ist die Farbe der Karte, die der Spieler
+     *                  gew�hlt hat.
      */
-	public MsgSelection(int selection) {
+	public MsgSelection(Colour selection) {
         this.selection = selection;
     }
 
     /**
-     * Diese Methode gibt die Auswahl des Spieler zurück, die er
+     * Diese Methode gibt die Farbauswahl des Spieler zur�ck, die er
      * gemacht hat.
-     * @return die Auswahl.
+     * @return die gewählte Farbe.
      */
-    public int getSelection() {
+    public Colour getSelection() {
         return selection;
     }
 }
