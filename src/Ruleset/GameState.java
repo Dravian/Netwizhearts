8f08d1e68f1dcbe0ef06520c14dfe1e0ff53679b
Daniel <dariedl14@yahoo.de>
2013-11-12 21:15:39 +0100

    Doku

--- src/Ruleset/GameState.java
+++ src/Ruleset/GameState.java
@@ -102,14 +102,6 @@ public class GameState {
 	}
 	
 	/**
-	 * Setzt die Spielrundenanzahl
-	 * @param Die eine neue Rundenanzahl
-	 */
-	protected void setRoundNumber(int number) {
-		
-	}
-	
-	/**
 	 * Setzt einen neuen Spieler als currentPlayer
 	 * @param player Der neue currentPlayer
 	 */
