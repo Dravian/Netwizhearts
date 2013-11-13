fd12eeea69bf31482c71c47095f7ec4d5e01e9f4
pkubin <kubin@fim.uni-passau.de>
2013-11-13 11:52:57 +0100

    main

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -14,8 +14,12 @@ public class ClientMain {
 	private ClientController clientController;
 	
 	/**
+	 * Diese Mainmethode wird beim Start des Programmes wie
+	 * beim Start eines jeden anderen Programmes auch vom Betriebssystem
+	 * bzw der JavaVM als Einsprungsspunkt in den Prozess verwendet.
+	 * Diese Mainmethode ist Fun and Awesome to call!
 	 * 
-	 * @param args
+	 * @param args Argumente die nicht verwendet werden.
 	 */
 	public static void main(final String[] args){
 	}
