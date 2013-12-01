c095239c9ba13a445cd1d67a4d350308994946bb
Daniel Riedl <dariedl14@yahoo.de>
2013-12-01 14:01:16 +0100

    Kleine Änderung

--- src/Ruleset/RulesetException.java
+++ src/Ruleset/RulesetException.java
@@ -1,16 +1,12 @@
 package Ruleset;
 
 /**
- * Wird geworfen wenn es im Regelwerk zu einem Fehler kommt
+ * Wird geworfen wenn im Ruleset selbst ein Fehler passiert
  */
-@SuppressWarnings("serial")
 public class RulesetException extends RuntimeException {
 
-	public RulesetException(){
-		super();
+	public RulesetException(String string) {
+		super(string);
 	}
-	
-	public RulesetException(String s){
-		super(s);
-	}
+
 }
