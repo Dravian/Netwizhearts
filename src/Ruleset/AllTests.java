c2942b2c04184b50b64b987292c99704baa25efb
alinamx <alina@meixl.de>
2014-01-06 20:47:04 +0100

    Tests

---
+++ src/Ruleset/AllTests.java
@@ -0,0 +1,18 @@
+package Ruleset;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TestClientHeartsIsValidMove.class, TestClientWizardIsValidMove.class,
+        TestHeartsCalculateTricks.class, TestHeartsIsValidMove.class,
+        TestHeartsIsValidMoveOnly.class, TestHeartsResolveMsgCard.class, TestHeartsRoundEnd.class,
+        TestHeartsSwap.class, TestHeartsWinner.class, TestisValidMoveHearts.class,
+        TestisValidMoveOnlyHearts.class, TestisValidMoveWizard.class,
+        TestRoundEndHearts.class, TestRoundEndWizard.class, TestStartWizard.class,
+        TestWizardCalculateTricks.class, TestWizardIsValidMove.class,
+        TestWizardResolveColour.class, TestWizardResolveMsgCard.class,
+        TestWizardResolveMsgNumber.class, TestWizardRoundEnd.class, TestWizardStart.class})
+ public class AllTests {
+ }
