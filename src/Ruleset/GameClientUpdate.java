fce6ccb3734c8a83d75edb79de69f595938c831e
Daniel Riedl <dariedl14@yahoo.de>
2014-01-17 16:25:08 +0100

    NoSerializableCardsTest

--- src/Ruleset/GameClientUpdate.java
+++ src/Ruleset/GameClientUpdate.java
@@ -3,7 +3,6 @@ package Ruleset;
 import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /** 
  * GameClientUpdate. Das GameClientUpdate wird vom RuleSet ueber den GameServer an den Client geschickt 
